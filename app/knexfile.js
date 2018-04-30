77a015a0df2e32ed6ec284e985877a895b3d0faa
Jordy van den Aardweg <jordyvandenaardweg@gmail.com>
2018-04-30 13:50:16 +0200

    Test db environment is ssl

--- app/knexfile.js
+++ app/knexfile.js
@@ -20,28 +20,10 @@ module.exports = {
     }
   },
 
-  staging: {
-    client: 'postgresql',
-    debug: true,
-    ssl: true,
-    connection: process.env.DATABASE_URL,
-    pool: {
-      min: 2,
-      max: 15
-    },
-    migrations: {
-      directory: './database/migrations',
-      tableName: 'migrations'
-    },
-    seeds: {
-      directory: './database/seeds'
-    }
-  },
-
   test: {
     client: 'postgresql',
     debug: false,
-    ssl: false,
+    ssl: true,
     connection: process.env.DATABASE_URL,
     pool: {
       min: 2,
